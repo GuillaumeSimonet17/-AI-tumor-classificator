4b6d10bee53487f9b3777728a00da6f469c359e6
Guillaume Simonet <guillaume@krafter.io>
2024-07-07 11:33:40 +0200

    update todos

--- main.py
+++ main.py
@@ -28,6 +28,31 @@ if __name__ == '__main__':
     params = train.train(X.T, y, neural_network)
     np.savez('datas/params', **params)
 
-# TODO: training/validation accuracy plot
+
+# 1 - split programm use seed
+# 2 - training programm print perf during training at each epoch
+# 3 - at the end of training, plot loss and acc for train and valid
+# 4 - predict take one exemple and calcul error function
+
+# TODO: To visualize your model performances during training, you will display at each epoch
+# the training and validation metrics.
+# x_train shape : (342, 30)
+# x_valid shape : (85, 30)
+# epoch 01/70 - loss: 0.6882 - val_loss: 0.6788
+
+# Enfaite, dans predict j'ai mis validation data mais, ce program doit attendre qu'un exemple,
+# pas une centaine..
+# Enfaite, validation sert dans le training :
+# y_pred_val = predict(X_val, params)
+#         val_loss.append(log_loss(y_val, y_pred_val.T))
+#         val_accuracy.append(calculate_accuracy(y_val, y_pred_val.T))
+# c'est tout !
+
+# TODO: Donc faut que je refasse mon predict.py pour faire en sorte qu'il predise pour un seul exemple
+# TODO: le predict.py doit evaluer la prediction using the binary cross-entropy error function.
+
+# TODO: pour split je dois utiliser seed pour avoir les mêmes resultats
+# TODO: training/validation accuracy plot (not really what I expected)
+
 # TODO: weights_initializer='heUniform')
 # TODO: faire softmax
